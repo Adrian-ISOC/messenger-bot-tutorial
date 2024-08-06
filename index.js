0c649b6d3fbf5a7112f2d378a979cff0f0490433
Adrian De Oleo C <43725706+Adrian-ISOC@users.noreply.github.com>
2024-08-06 09:27:37 -0400

    added new token

--- index.js
+++ index.js
@@ -1,145 +1,15 @@
-//This is still work in progress
-/*
-Please report any bugs to nicomwaks@gmail.com
+'use strict';
+const BootBot = require('bootbot');
 
-i have added console.log on line 48 
+const bot = new BootBot({
+  accessToken: 'EAAG3z4f9pZAYBOZBZC5ZCN60WT5ZAX6DqiUgPr7lcJYutFmgIVc8hs7zpjhs5kdYhFyO17Ok36YmZAdNZAXTIwPSOiQNG6pzKZChbfaaEQ8F8ACvhWOIUuBc6JatjNyMUV51WMGNW37vXk85y2StGw5uGtyKLiZCucCU5IJ2PCGYqKxhCqmCfzernoQHakLb8vmJU4ZBL2UAKyeAZDZD',
+  verifyToken: 'thisisasampletoken',
+  appSecret: '5f77b2954c9721aa700a609f75af126a'
+});
 
+bot.on('message', (payload, chat) => {
+  const text = payload.message.text;
+  chat.say(`Echo: ${text}`);
+});
 
-
-
- */
-'use strict'
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-
-app.set('port', (process.env.PORT || 5000))
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// index
-app.get('/', function (req, res) {
-	res.send('hello world i am a secret bot')
-})
-
-// for facebook verification
-app.get('/webhook/', function (req, res) {
-	console.log(req.query['hub.verify_token'])
-	if (req.query['hub.verify_token'] === '5f77b2954c9721aa700a609f75af126a') {
-		res.send(req.query['hub.challenge'])
-	} else {
-		res.send('Error, wrong token')
-	}
-})
-//EAAG3z4f9pZAYBO9ClSQgBesCpfUPsLL2e0GJT2yn3pDZCJM8nr2b2lWBWkOzDgnf2wDCqJ2nZCIBbIG0uW9Kz1aOUPuOppegde7WjnKgUfJMH0nN5Cp3lg9JBc07VzzWjLCpsOurzEZCfPvIvRsaZCPp5Qul7XHe0w0AEJzpmwhfg5HYA0nmEdT7Xj0q2kXDwlHlFgOll6gZDZD
-// to post data
-app.post('/webhook/', function (req, res) {
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = req.body.entry[0].messaging[i]
-		let sender = event.sender.id
-		if (event.message && event.message.text) {
-			let text = event.message.text
-			if (text === 'Generic'){ 
-				console.log("welcome to chatbot")
-				sendGenericMessage(sender)
-				continue
-			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-		}
-		if (event.postback) {
-			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-			continue
-		}
-	}
-	res.sendStatus(200)
-})
-
-
-// recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = "EAAG3z4f9pZAYBO9ClSQgBesCpfUPsLL2e0GJT2yn3pDZCJM8nr2b2lWBWkOzDgnf2wDCqJ2nZCIBbIG0uW9Kz1aOUPuOppegde7WjnKgUfJMH0nN5Cp3lg9JBc07VzzWjLCpsOurzEZCfPvIvRsaZCPp5Qul7XHe0w0AEJzpmwhfg5HYA0nmEdT7Xj0q2kXDwlHlFgOll6gZDZD"
-
-function sendTextMessage(sender, text) {
-	console.log(sender, text)
-	let messageData = { text:text }
-	request({
-		url: 'https://graph.facebook.com/v20.0/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			messageType: "RESPONSE",
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		console.log(error, response, body);
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	}
-	request({
-		url: 'https://graph.facebook.com/v20.0/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		console.log(error, response, body)
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-// spin spin sugar
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+bot.start();
