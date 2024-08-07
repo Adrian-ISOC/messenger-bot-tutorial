2ca9da19df8010b50dba7e760d506fb3f2b95ebe
Adrian De Oleo C <43725706+Adrian-ISOC@users.noreply.github.com>
2024-08-06 20:28:17 -0400

    added sample token

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const bot = new BootBot({
   accessToken: 'EAAG3z4f9pZAYBOZBZC5ZCN60WT5ZAX6DqiUgPr7lcJYutFmgIVc8hs7zpjhs5kdYhFyO17Ok36YmZAdNZAXTIwPSOiQNG6pzKZChbfaaEQ8F8ACvhWOIUuBc6JatjNyMUV51WMGNW37vXk85y2StGw5uGtyKLiZCucCU5IJ2PCGYqKxhCqmCfzernoQHakLb8vmJU4ZBL2UAKyeAZDZD',
   verifyToken: 'thisisasampletoken',
   appSecret: '5f77b2954c9721aa700a609f75af126a'
-});
+}); 
+
+
 
 bot.on('message', (payload, chat) => {
   const text = payload.message.text;
