47a401937878dbf986af0b96cd724efa5ae68320
Adrian De Oleo C <43725706+Adrian-ISOC@users.noreply.github.com>
2024-08-06 20:05:28 -0400

    adedd

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ bot.on('message', (payload, chat) => {
 });
 
 bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+  console.log('payload', payload);
 	// Send a text message followed by another text message that contains a typing indicator
 	chat.say('Hello, human friend!').then(() => {
 		chat.say('How are you today?', { typing: true });
