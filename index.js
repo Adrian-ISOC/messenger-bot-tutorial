55d4854fd683344d35f55dd4102329ff975c3813
Adrian De Oleo C <43725706+Adrian-ISOC@users.noreply.github.com>
2024-08-06 20:33:46 -0400

    added access token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const BootBot = require('bootbot');
 
 const bot = new BootBot({
-  accessToken: 'EAAG3z4f9pZAYBOZBZC5ZCN60WT5ZAX6DqiUgPr7lcJYutFmgIVc8hs7zpjhs5kdYhFyO17Ok36YmZAdNZAXTIwPSOiQNG6pzKZChbfaaEQ8F8ACvhWOIUuBc6JatjNyMUV51WMGNW37vXk85y2StGw5uGtyKLiZCucCU5IJ2PCGYqKxhCqmCfzernoQHakLb8vmJU4ZBL2UAKyeAZDZD',
+  accessToken: 'EAAG3z4f9pZAYBOwhadtT6wvIl1vMA8UBszErT7h2nWW3OzkVDMk11ZBtkQwRwOxSvhatozlZAtSUxc5ImZAlIYvkACsyWOyx7suNQyHeNH8YTTM4M3c3ZBmynQuYOcE8LvfzgQDFCdPjZCVATZCmRk4vzXPz2xRrynJEQjlQW9K2yc0KedVmHM6dZCvKGuzRBQ3hsEE4zXCo8QZDZD',
   verifyToken: 'thisisasampletoken',
   appSecret: '5f77b2954c9721aa700a609f75af126a'
 }); 
